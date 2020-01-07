7947e466e9f89b09a5020cd140a56115a03d3c9a
Mauro Delazeri <maurodelazeri@gmail.com>
2020-01-06 19:19:19 -0500

    changes

--- src/zinnionAPI.cpp
+++ src/zinnionAPI.cpp
@@ -8,4 +8,8 @@ std::vector<int> simulate_student() {
 void test() {
   cout << "mauro" << endl;
   cout << add_int(1,3) << endl;
+  char *a = "abc"; // valid and safe in either C or C++.
+  char *b = "abc"; // valid and safe in either C or C++.
+  char *c = "trade:COINBASEPRO:BTC-USD"; // valid and safe in either C or C++.
+  init(a,b,c);
 }
