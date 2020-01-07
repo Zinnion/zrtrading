b39392ba824733ce78c0f4437b2bcb181e5dc5ac
Mauro Delazeri <maurodelazeri@gmail.com>
2020-01-06 19:40:39 -0500

    changes

--- src/zinnionAPI.h
+++ src/zinnionAPI.h
@@ -6,8 +6,16 @@
 using namespace Rcpp;
 using namespace std;
 
+//' Multiply a number by two
+//'
+//' @param x A single integer.
+//' @export
 // [[Rcpp::export]]
 void test();
 
+//' Multiply a number by two
+//'
+//' @param x A single integer.
+//' @export
 // [[Rcpp::export]]
 std::vector<int> simulate_student();
